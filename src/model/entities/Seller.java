de39252ff91d59d586d1e57fa0dfbcf262938e0e
Jeferson Monteiro <monteirao.jeff@gmail.com>
2022-08-12 17:44:49 -0300

    DepartmentDao and SellerDao interfaces

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -14,20 +14,20 @@ public class Seller implements Serializable {
 	private Date birthDate;
 	private Double baseSalary;
 	
-	private Departament departament;
+	private Department department;
 	
 	public Seller() {
 		
 	}
 
-	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Departament departament) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department departament) {
 		
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
-		this.departament = departament;
+		this.department = departament;
 	}
 
 	public Integer getId() {
@@ -70,12 +70,12 @@ public class Seller implements Serializable {
 		this.baseSalary = baseSalary;
 	}
 
-	public Departament getDepartament() {
-		return departament;
+	public Department getDepartment() {
+		return department;
 	}
 
-	public void setDepartament(Departament departament) {
-		this.departament = departament;
+	public void setDepartment(Department departament) {
+		this.department = departament;
 	}
 
 	@Override
@@ -98,7 +98,7 @@ public class Seller implements Serializable {
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + ", departament=" + departament + "]";
+				+ baseSalary + ", departament=" + department + "]";
 	}
 	
 	
